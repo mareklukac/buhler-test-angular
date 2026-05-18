96f7cf8dc43f7480bfdddc47f1437ae22af7dce4
Marek Lukac <marek.lukac08@gmail.com>
2026-05-18 21:31:40 +0200

    feat: Add overview component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from './header/header';
 import { Nav } from './nav/nav';
+import { Overview } from './overview/overview';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Nav],
+  imports: [Header, Nav, Overview],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
---
+++ src/app/overview/overview.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-overview',
+  imports: [MatIconModule],
+  templateUrl: './overview.html',
+  styleUrl: './overview.scss',
+})
+export class Overview {}
