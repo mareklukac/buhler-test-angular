71184b2b6f0cf510bb8dd694f412587d22b49f71
Marek Lukac <marek.lukac08@gmail.com>
2026-05-18 22:48:42 +0200

    feat: Add dark mode

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,16 +1,24 @@
 import { DatePipe } from '@angular/common';
-import { Component, DestroyRef, effect, inject, signal } from '@angular/core';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-header',
-  imports: [DatePipe, MatIconModule],
+  imports: [DatePipe, MatIconModule, MatButtonModule, MatTooltipModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
   private destroyRef = inject(DestroyRef);
   time = signal(new Date());
+  isDark = signal(false);
+
+  toggleTheme() {
+    this.isDark.update((v) => !v);
+    document.body.classList.toggle('dark', this.isDark());
+  }
 
   constructor() {
     const timeInterval = setInterval(() => {
