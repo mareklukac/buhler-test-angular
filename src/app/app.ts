01b1af7150b69964e198d5eb21e6899eaf48e109
Marek Lukac <marek.lukac08@gmail.com>
2026-05-18 21:05:54 +0200

    feat: Create nav component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Header } from './header/header';
+import { Nav } from './nav/nav';
 
 @Component({
   selector: 'app-root',
-  imports: [Header],
+  imports: [Header, Nav],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
---
+++ src/app/nav/nav.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-nav',
+  imports: [MatIconModule],
+  templateUrl: './nav.html',
+  styleUrl: './nav.scss',
+})
+export class Nav {}
